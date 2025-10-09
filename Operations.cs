373df87e3861c51cc9f023a0407df6d94c36d791
Justincase725 <justin.robinson@my.pbsc.edu>
2025-10-09 09:30:39 -0400

    Create Operations.cs

---
+++ Operations.cs
@@ -0,0 +1,25 @@
+/*
+ * File: Operations.cs
+ * 
+ * Role C – Nayobe Jean-Louis (CRUD Developer)
+ * Role D – Leopoldo Ramos (Extras Developer)
+ * Role E – Donovan Zangwill (Display & Documentation)
+ * Role A – Justin Robinson (Lead / Repo Owner)
+ * Role B – Robert Weinberger (Menu Developer)
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Module7DictionaryProject
+{
+    internal class Operations
+    {
+        public static Dictionary<string, List<string>> myDictionary = new();
+
+        public static void Placeholder()
+        {
+            Console.WriteLine("[Operations placeholder – Roles C, D, and E will implement methods here]");
+        }
+    }
+}
