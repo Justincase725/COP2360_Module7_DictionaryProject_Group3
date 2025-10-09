52bc628c33d78dfc68dbf943987ab809e82b7327
Justincase725 <justin.robinson@my.pbsc.edu>
2025-10-09 09:30:13 -0400

    Create Menu.cs

---
+++ Menu.cs
@@ -0,0 +1,22 @@
+/*
+ * File: Menu.cs
+ * 
+ * Role B – Robert Weinberger (Menu Developer)
+ * Role A – Justin Robinson (Lead / Repo Owner)
+ * Role C – Nayobe Jean-Louis (CRUD Developer)
+ * Role D – Leopoldo Ramos (Extras Developer)
+ * Role E – Donovan Zangwill (Display & Documentation)
+ */
+
+using System;
+
+namespace Module7DictionaryProject
+{
+    internal class Menu
+    {
+        public static void DisplayPlaceholder()
+        {
+            Console.WriteLine("[Menu placeholder – to be implemented by Role B (Robert Weinberger)]");
+        }
+    }
+}
